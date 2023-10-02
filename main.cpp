6c5a6899e9b2b30b6c5ae988404cc253c4f1a887
Daniel Ordaz <dordaz2@ucmerced.edu>
2023-10-02 20:52:29 +0000

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "wordle_functions.h"
+
+using namespace std;
+
+//git init
+//git add .
+//git commit -m "message"
+
+int main(int argc, char* argv[]) {
+    /*
+        your code goes here
+    */
+
+    return 0;
+}
---
+++ wordle_functions.h
@@ -0,0 +1,12 @@
+#ifndef WORDLE_FUNCTIONS_H
+#define WORDLE_FUNCTIONS_H
+
+#include<vector>
+#include<iostream>
+
+int test(){
+    return 1;
+}
+// Include functions here
+
+#endif
