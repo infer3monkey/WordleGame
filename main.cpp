346670ba1955480de9455d956045bf4a629e70e6
Daniel Ordaz <dordaz2@ucmerced.edu>
2023-10-03 18:05:39 +0000

    implemented contains function

--- main.cpp
+++ main.cpp
@@ -36,36 +36,32 @@ int main(int argc, char* argv[]) {
 
     while (notfinished){
         solution = realsolution;
+        cout << "Your input: ";
         cin >> attempt;
-        if (attempt.length() != 5 && containsList(attempt, allowedlist)){
+
+        if (attempt.length() != 5){
             cout << "invalid length try again" << endl;
+        } else if (containsList(attempt, allowedlist)!= true){
+            cout << "invalid word try again" << endl;
         } else { //valid attempt. go through every letter and give back the right colors
-            for(int i = 0; i < 5;i++){ // SLATE and SLASA as the guess would only have first s as green
-                //SLATE AND ASASA as the guess would only have the first s as yellow
-                //use a place holder
+            for(int i = 0; i < 5;i++){ 
                 if(attempt[i] == solution[i]){//green
                     cout << green(attempt[i]);
                     solution[i] = '-';
-                } else if (contains(attempt[i], solution) != -1){
-                    //yellow
+                } else if (contains(attempt[i], solution) != -1){//yellow
                     cout << yellow(attempt[i]);
                     solution[contains(attempt[i], solution)] = '-';
-                    //solution[i] = '-'; need to find where the thing
                 } else { //gray
                     cout << gray(attempt[i]);
                 }
             }
+            cout << endl;
         }
         if (attempt == realsolution){
             cout << "\nCongrats you solved the Wordle!" << endl;
             notfinished = false;
         }
-        cout << endl;
     }
-    
-    
-
-    //cout << yellow(attempt) << endl;
 
     return 0;
 }
