bb35b0a4a6448970818fb711102d0f1447300342
Daniel Ordaz <dordaz2@ucmerced.edu>
2023-10-07 01:12:24 +0000

    made wordle game replayable

--- main.cpp
+++ main.cpp
@@ -48,8 +48,44 @@ int main(int argc, char* argv[]) {
         getline(cin, playerInput);
 
         if (stoi(playerInput) == 1){//player wants to play wordle, breaks the program if not integer
-            mainmenubool = false;
+            //mainmenubool = false;
             cout << "\nYou Are Now Playing Wordle. You Have 6 Valid Attempts" << endl;
+            for(int i = 0; i < 6; i++){//wordle game
+                getline(cin, attempt);
+
+                bool inWords = containsList(attempt, wordlist);
+
+                if (attempt.length() != 5){
+                    cout << "invalid length try again" << endl;
+                    i--;//invalid attempt
+                } else if (inWords == false){
+                    cout << "invalid word try again" << endl;
+                    i--;//invalid attempt
+                } else { //valid attempt return the word with correct colors
+                    attemptlist.push_back(wordleGame(attempt, solution));
+                    for(int i = 0; i < attemptlist.size();i++){
+                        cout << attemptlist[i] << endl;
+                    }
+                }
+                if (attempt == solution){
+                    cout << "\nCongrats you solved the Wordle in " << i+1 << " attempts!" << endl;
+                    won = true;
+                    break;
+                }
+            }
+            if (won == false){//loss message
+                cout << "You did not complete the Wordle in 6 attempts. The word was: " << solution << endl;
+            }
+            bool wordlegamemenu = true;
+            cout << "Press [enter] to go back to the main menu\n";
+            while(wordlegamemenu){
+                getline(cin, playerInput);
+                if(playerInput == ""){
+                    wordlegamemenu = false;
+                }
+            }
+            cout << "\n" << mainmenu();
+
         } else if (stoi(playerInput) == 2) {//player wants to go to how to play screen
             cout << howtoplaymenu();
             bool howtoplaymenu = true;
@@ -60,39 +96,15 @@ int main(int argc, char* argv[]) {
                 }
             }
             cout << "\n" << mainmenu();
+        } else if (stoi(playerInput) == 6){
+            mainmenubool = false;
         } else {
             cout << "not a proper option or have not created it\nSelect a number:";
         }
         
     }
 
-    for(int i = 0; i < 6; i++){//wordle game
-        getline(cin, attempt);
-
-        bool inWords = containsList(attempt, wordlist);
-
-        if (attempt.length() != 5){
-            cout << "invalid length try again" << endl;
-            i--;//invalid attempt
-        } else if (inWords == false){
-            cout << "invalid word try again" << endl;
-            i--;//invalid attempt
-        } else { //valid attempt return the word with correct colors
-            attemptlist.push_back(wordleGame(attempt, solution));
-            for(int i = 0; i < attemptlist.size();i++){
-                cout << attemptlist[i] << endl;
-            }
-        }
-        if (attempt == solution){
-            cout << "\nCongrats you solved the Wordle in " << i+1 << " attempts!" << endl;
-            won = true;
-            break;
-        }
-    }
-
-    if (won == false){//loss message
-        cout << "You did not complete the Wordle in 6 attempts. The word was: " << solution << endl;
-    }
+    
     
 
     return 0;
