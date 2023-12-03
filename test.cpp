09beddfe657ccd5bc10abc3e9aff1158487e5299
Daniel Ordaz <dordaz2@ucmerced.edu>
2023-12-03 23:27:45 +0000

    created one test and started on another

--- test.cpp
+++ test.cpp
@@ -1,5 +1,10 @@
 #include <igloo/igloo.h>
 #include "wordle_functions.h"
+#define GREEN "\033[32m"
+#define GRAY "\033[37m"
+#define YELLOW "\033[33m"
+#define BOLD "\033[1m"
+#define RESET "\033[0m"
 
 using namespace igloo;
 
@@ -20,6 +25,26 @@ Context(TODO){
         stringvec.push_back("hello");
         bool actual = containsList(hello, stringvec);
         bool expected = true;
+        Assert::That(actual, Equals(expected));
+    }
+    Spec(helloisallgreen){//test for input of hello when the solution is hello
+        std::string actual = wordleGame("hello", "hello");
+        std::string expected;
+        expected += GREEN"/---\\" RESET GREEN"/---\\" RESET GREEN"/---\\" RESET GREEN"/---\\" RESET GREEN"/---\\" RESET;
+        expected += "\n";
+        expected += GREEN"| h |" RESET GREEN"| e |" RESET GREEN"| l |" RESET GREEN"| l |" RESET GREEN"| o |" RESET;
+        expected += "\n";
+        expected += GREEN"\\---/" RESET GREEN"\\---/" RESET GREEN"\\---/" RESET GREEN"\\---/" RESET GREEN"\\---/" RESET;
+        Assert::That(actual, Equals(expected));
+    }
+    Spec(slateandstate){//test for slate and state
+        std::string actual = wordleGame("state", "slate");
+        std::string expected;
+        expected += GREEN"/---\\" RESET GRAY"/---\\" RESET GREEN"/---\\" RESET GREEN"/---\\" RESET GREEN"/---\\" RESET;
+        expected += "\n";
+        expected += GREEN"| s |" RESET GRAY"| l |" RESET GREEN"| a |" RESET GREEN"| t |" RESET GREEN"| e |" RESET;
+        expected += "\n";
+        expected += GREEN"\\---/" RESET GRAY"\\---/" RESET GREEN"\\---/" RESET GREEN"\\---/" RESET GREEN"\\---/" RESET;
         Assert::That(actual, Equals(expected));
     }
 };
