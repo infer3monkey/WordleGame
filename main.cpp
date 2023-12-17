c1752d8284857b438f0cf49d4bfa44709068b08a
Daniel Ordaz <dordaz2@ucmerced.edu>
2023-12-17 19:07:58 +0000

    Clean up folders and files

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "wordle_functions.h"
-#include <fstream>
-#include <ctype.h>
-using namespace std;
-int main(int argc, char* argv[]) {
-    entirewordlegame();
-    return 0;
-}
